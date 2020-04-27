d4ac24388fae8e4f993603af4e7bc922a583ff56
Nguyen Hoang Tue <tuerong0301@gmail.com>
2020-04-27 17:22:01 +0900

    1234

---
+++ Practices/Problem2.py
@@ -0,0 +1,13 @@
+print('問題２(Dランク):メダリストの表示')
+print('URL:https://paiza.jp/challenges/140/show')
+print("________________________________________")
+
+
+def result(a,b):
+#a is medal,b is name
+    print(f'{a}:{b}')
+
+result("Gold","John")
+result("Silver","Danny")
+result("Bronze","James")
+
---
+++ Practices/Problem3.py
@@ -0,0 +1,14 @@
+print("問題３(Dランク)：台風の間隔")
+print("URL:https://paiza.jp/challenges/142/show")
+print("______________________________")
+
+n1=int(input("n1"))
+n2=int(input("n2"))
+n3=int(input("n3"))
+n4=int(input("n4"))
+n5=int(input("n5"))
+
+print(n2-n1)
+print(n3-n2)
+print(n4-n3)
+print(n5-n4)
